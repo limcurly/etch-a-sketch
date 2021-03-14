fe22ca5467ea0ece3af2d19caccf6ece03a4104d
limcurly <liamcarla00@gmail.com>
2021-03-14 14:42:41 +1100

    added main elements

---
+++ README.md
@@ -0,0 +1,5 @@
+Etch a sketch to be used in browser, created with JS.
+
+
+v1:
+added title and grid.
---
+++ main.js
@@ -0,0 +1,5 @@
+for (let i = 0; i < 256; i++) {
+    let gridTile = document.createElement('div');
+    gridTile.className = "gridTile";
+    container.appendChild(gridTile);
+};
