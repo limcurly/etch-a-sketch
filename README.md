6cd83b01c7de48f313a36e72991709a36799267e
limcurly <liamcarla00@gmail.com>
2021-03-17 00:47:26 +1100

    added drawing and reset button

--- README.md
+++ README.md
@@ -3,3 +3,13 @@ Etch a sketch to be used in browser, created with JS.
 
 v1:
 added title and grid.
+
+
+
+v1.1:
+added drawing and reset button;
+
+TODO: 
+
+MAKE RESET BUTTON RESET TO GRIDS
+MAKE REESET BUTTON PROMPT USER TO CHOOSE GRID PERIMETER;
--- main.js
+++ main.js
@@ -1,5 +1,30 @@
+//CREATE GRIDS AND ALLOW USER TO "DRAW" WHEN HOVERED OVER
 for (let i = 0; i < 256; i++) {
-    let gridTile = document.createElement('div');
+    const gridTile = document.createElement('div');
     gridTile.className = "gridTile";
     container.appendChild(gridTile);
-};
+
+    gridTile.addEventListener('mouseover', () => {
+        gridTile.style.background = '#348bba';
+    });
+}; 
+
+
+document.getElementById('reset-btn').addEventListener
+('click', () => {
+    
+    let userGridInput = prompt('How many square per side in your new grid, good sir/madam?');
+
+    if (userGridInput < 1 || userGridInput > 100 || userGridInput == "" ) {
+        alert("Please choose a size between 1 and 100")
+    } else if (userGridInput === null) {
+        location.reload();
+    }
+
+    console.log(userGridInput)
+});
+
+
+
+
+
